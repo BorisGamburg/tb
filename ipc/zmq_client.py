0d29b2783a16ee0b84ebb96b75545f5046cdc506
BorisGamburg <borisgamburg65@gmail.com>
2026-08-28 17:58:01 +0200

    Raise timeout error from ZmqClient

--- ipc/zmq_client.py
+++ ipc/zmq_client.py
@@ -7,13 +7,15 @@ class ZmqClient:
         self.socket = self.context.socket(zmq.REQ)
         self.socket.connect(endpoint)
 
-    def request(self, command: dict, timeout: float) -> dict | None:
+    def request(self, command: dict, timeout: float) -> dict:
         self.socket.send_json(command)
 
         if self.socket.poll(int(timeout * 1000)):
             return self.socket.recv_json()
 
-        return None
+        raise TimeoutError(
+            f"Timeout waiting for response ({timeout}s)"
+        )
 
     def close(self):
         self.socket.close()
